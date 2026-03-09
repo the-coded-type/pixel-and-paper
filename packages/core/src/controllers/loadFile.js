5835423db7f108b723917bdaecbf2dc2c2c24b92
Victor Baquero Wihlborg <v@vbw.se>
2026-03-09 14:35:30 +0100

    Add ability to load images

--- packages/core/src/controllers/loadFile.js
+++ packages/core/src/controllers/loadFile.js
@@ -1,14 +1,16 @@
-// Simple file laoder
+// Simple file loader
 export const loadFile = async (filePath) => {
-    try {
-        // Request the file from the local server
-        const response = await fetch(filePath);
-
-        // Convert the response to text
-        const content = await response.text();
-
-        return content;
-    } catch (err) {
-        console.error("Error reading file:", err);
+  try {
+    // Request the file from the server
+    const response = await fetch(filePath);
+    if (response.headers.get("content-type").includes("image")) {
+      // Convert image file response to blob
+      return await response.blob();
+    } else {
+      // Convert the response to text
+      return await response.text();
     }
+  } catch (err) {
+    console.error("Error reading file:", err);
+  }
 };
